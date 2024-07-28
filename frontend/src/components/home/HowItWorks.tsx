a7955c747a0eacf6e2aa8e5241c65d27f7a377db
signor1 <emmanuelomemgboji@gmail.com>
2024-07-28 20:45:40 +0100

    feat: added more sections

--- frontend/src/components/home/HowItWorks.tsx
+++ frontend/src/components/home/HowItWorks.tsx
@@ -8,7 +8,7 @@ const HowItWorks = () => {
     const steps: { caption: string, text: string, icon: JSX.Element }[] = [
         {
             caption: "Authenticate Your Account",
-            text: "Before you can start streaming tokens, securely authenticate your account using industry-standard authentication methods. Ensure the safety of your financial data with TRiver's robust security measures.",
+            text: "Before you can start streaming tokens, securely authenticate your account using industry-standard authentication methods. Ensure the safety of your financial data with Flow8 robust security measures.",
             icon: <SlUserFollowing />,
         }, {
             caption: "Set Up Your Stream",
@@ -20,34 +20,34 @@ const HowItWorks = () => {
             icon: <CiEdit />,
         }, {
             caption: "Activate Your Stream",
-            text: "Once you've configured your stream to your satisfaction, activate it to start the token flow. Sit back and watch as TRiver handles the rest, ensuring that your tokens are distributed smoothly and efficiently according to your predefined criteria.",
+            text: "Once you've configured your stream to your satisfaction, activate it to start the token flow. Sit back and watch as Flow8 handles the rest, ensuring that your tokens are distributed smoothly and efficiently according to your predefined criteria.",
             icon: <SlPower />,
         }, {
             caption: "Manage Your Stream",
-            text: "Monitor and manage your token stream with ease using TRiver's intuitive interface. Pause, modify, or stop your stream as needed, and track your transaction history to stay informed about your financial activities.",
+            text: "Monitor and manage your token stream with ease using Flow8 intuitive interface. Pause, modify, or stop your stream as needed, and track your transaction history to stay informed about your financial activities.",
             icon: <LuSettings2 />,
         }, {
             caption: "Enjoy Seamless Automation",
-            text: "With TRiver, you can enjoy the benefits of seamless financial automation without the hassle of manual intervention. Whether you're distributing salaries, rewarding users, or managing subscriptions, TRiver streamlines the process for maximum efficiency and convenience.",
+            text: "With Flow8, you can enjoy the benefits of seamless financial automation without the hassle of manual intervention. Whether you're distributing salaries, rewarding users, or managing subscriptions, Flow8 streamlines the process for maximum efficiency and convenience.",
             icon: <RxUpdate />,
         }
     ]
     return (
         <main className='w-full flex flex-col items-center md:my-24 my-20 lg:px-20 md:px-16 px-4'>
-            <h1 className="text-4xl text-gray-200 font-belanosima">How It Works</h1>
-            <p className="text-gray-400 font-barlow text-center md:w-[50%]">TRiver makes token streaming easy and efficient, allowing you to automate financial processes with just a few simple steps:</p>
+            <h1 className="text-4xl text-color5 font-belanosima">How It Works</h1>
+            <p className="text-gray-500 font-barlow text-center md:w-[50%]">Flow8 makes token streaming easy and efficient, allowing you to automate financial processes with just a few simple steps:</p>
             <div className="w-full  grid lg:grid-cols-3 md:grid-cols-2 gap-10 mt-12">
                 {
                     steps.map(({ caption, text, icon }, index) => (
-                        <div className={`flex flex-col bg-gray-900 px-6 py-8 items-center gap-4 overflow-hidden relative cursor-pointer`} key={index}>
-                            <div className="  absolute -top-10 -left-7 w-[70px] h-[100px] bg-gray-300 rotate-45 flex justify-end items-center pr-4">
-                                <h1 className="-rotate-45 text-2xl text-gray-900 font-barlow">{index + 1}</h1>
+                        <div className={`flex flex-col bg-color5 px-6 py-8 items-center gap-4 overflow-hidden relative cursor-pointer`} key={index}>
+                            <div className="  absolute -top-10 -left-7 w-[70px] h-[100px] bg-color2 rotate-45 flex justify-end items-center pr-4">
+                                <h1 className="-rotate-45 text-2xl text-color5 font-barlow">{index + 1}</h1>
                             </div>
-                            <div className="w-[100px] h-[100px] rounded-full bg-gradient-to-br from-sky-600 to-emerald-400 flex justify-center items-center text-3xl ">
+                            <div className="w-[100px] h-[100px] rounded-full bg-gradient-to-br from-color4 to-color5 text-white flex justify-center items-center text-3xl ">
                                 {icon}
                             </div>
                             <h3 className="text-xl text-gray-200 font-belanosima">{caption}</h3>
-                            <p className="text-gray-400 font-barlow">{text}</p>
+                            <p className="text-gray-400 text-sm font-barlow">{text}</p>
 
                         </div>
                     ))
--- frontend/src/components/home/WhyStream.tsx
+++ frontend/src/components/home/WhyStream.tsx
@@ -21,34 +21,34 @@ const WhyStream = () => {
         <main className='w-full flex flex-col items-start my-24'>
             <div className="w-full grid md:grid-cols-2 gap-20 lg:px-28 px-4">
                 <div className="w-full order-1 md:order-2 relative flex justify-between items-end">
-                    <div className="lg:w-[150px] lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-sky-600 to-emerald-400 flex justify-center items-center">
-                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-gray-950"></div>
+                    <div className="lg:w-[150px] lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-color4 to-color5 flex justify-center items-center">
+                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-color2"></div>
                     </div>
-                    <div className="lg:w-[150px] self-start md:mt-12 mt-8 lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-sky-600 to-emerald-400 flex justify-center items-center">
-                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-gray-950"></div>
+                    <div className="lg:w-[150px] self-start md:mt-12 mt-8 lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-color4 to-color5 flex justify-center items-center">
+                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-color2"></div>
                     </div>
-                    <div className="lg:w-[150px] lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-sky-600 to-emerald-400 flex justify-center items-center">
-                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-gray-950"></div>
+                    <div className="lg:w-[150px] lg:h-[150px] w-[100px] h-[100px] rounded-full bg-gradient-to-br from-color4 to-color5 flex justify-center items-center">
+                        <div className="lg:w-[100px] lg:h-[100px] w-[60px] h-[60px] rounded-full bg-color2"></div>
                     </div>
-                    <div className="lg:w-[350px] lg:h-[350px] w-[250px] h-[250px] absolute lg:left-20 left-12 bg-gradient-to-br from-sky-600 to-emerald-400 rounded-full  flex justify-center items-center">
-                        <div className="lg:w-[280px] lg:h-[280px] w-[190px] h-[190px] rounded-full bg-gray-950 flex justify-center items-center">
-                            <SiSpringsecurity className="lg:text-9xl text-7xl text-emerald-400" />
+                    <div className="lg:w-[350px] lg:h-[350px] w-[250px] h-[250px] absolute lg:left-20 left-12 bg-gradient-to-br from-color4 to-color5 rounded-full  flex justify-center items-center">
+                        <div className="lg:w-[280px] lg:h-[280px] w-[190px] h-[190px] rounded-full bg-color2 flex justify-center items-center">
+                            <SiSpringsecurity className="lg:text-9xl text-7xl text-color4" />
                         </div>
 
                     </div>
                 </div>
                 <div className="flex flex-col order-2 md:order-1 items-start gap-1.5">
-                    <h1 className="text-3xl text-gray-200 font-belanosima">Why Flow8</h1>
-                    <p className="text-gray-400 font-barlow">With TRiver, you can say goodbye to manual transaction processes and hello to a new era of financial automation. Whether you're an individual looking to streamline your personal finances or a business seeking to optimize your operations, TRiver offers the tools and capabilities you need to succeed in the world of DeFi.</p>
-                    <h3 className="text-xl text-gray-200 font-belanosima mt-4">Key Features</h3>
-                    <ul className="flex flex-col gap-2 text-gray-400 font-barlow">
+                    <h1 className="text-3xl text-color5 font-belanosima">Why Flow8</h1>
+                    <p className="text-gray-500 font-barlow">With Flow8, you can say goodbye to manual transaction processes and hello to a new era of financial automation. Whether you're an individual looking to streamline your personal finances or a business seeking to optimize your operations, Flow8 offers the tools and capabilities you need to succeed in the world of DeFi.</p>
+                    <h3 className="text-xl text-color5 font-belanosima mt-4">Key Features</h3>
+                    <ul className="flex flex-col gap-2 text-gray-500 font-barlow">
                         {
                             features.map(({ caption, text }, index) => (
                                 <li key={index} className="flex items-start gap-2">
-                                    <span className="text-emerald-400 mt-1.5">
+                                    <span className="text-color4 mt-1.5">
                                         <GiStarShuriken />
                                     </span>
-                                    <p className="text-gray-400"><span className="text-gray-300 font-belanosima">{caption}:</span> {text}</p>
+                                    <p className="text-gray-500"><span className="text-gray-500 font-belanosima">{caption}:</span> {text}</p>
                                 </li>
                             ))
                         }
